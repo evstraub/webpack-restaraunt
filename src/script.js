cf662a25c32f1e1249089e74392e095a5a58a5cb
Ethan Straub <evstraub@gmail.com>
2022-12-08 17:18:44 -0500

    added more js to the page to make it look more like a menu

--- src/script.js
+++ src/script.js
@@ -1,7 +1,27 @@
 export function initialPageLoad() {
   const contentDiv = document.querySelector("#content");
   const card = document.createElement("div");
-  const title = document.createElement("div");
+  const image = document.createElement("img");
+  const heading = document.createElement("h1");
+  heading.textContent = "Dib's";
+  card.appendChild(heading);
   contentDiv.append(card);
-  card.appendChild(title);
+  card.appendChild(image);
+  card.classList.add("menu-card");
+  image.classList.add("res-image");
+  image.src = "../src/res.jpg";
+  image.alt = "restaurant image";
+  contentDiv.appendChild(image);
+
+  const card1 = document.createElement("p");
+  card1.classList.add("card1");
+  card.appendChild(card1);
+
+  const card2 = document.createElement("p");
+  card2.classList.add("card2");
+  card.appendChild(card2);
+
+  const card3 = document.createElement("p");
+  card3.classList.add("card3");
+  card.appendChild(card3);
 }
