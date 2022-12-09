d4ce0fe679a01325f192b8849c2303ebcda8449c
Ethan Straub <evstraub@gmail.com>
2022-12-09 15:55:51 -0500

    added content divs

--- src/script.js
+++ src/script.js
@@ -6,16 +6,31 @@ export function initialPageLoad() {
   const button2 = document.createElement("button");
   const button3 = document.createElement("button");
   const title = document.createElement("p");
+  const sumCard = document.createElement("div");
+  const hoursCard = document.createElement("p");
+  const locCard = document.createElement("p");
 
   header.appendChild(button1);
   header.appendChild(button2);
   header.appendChild(button3);
   card.appendChild(header);
   card.appendChild(title);
+  card.appendChild(sumCard);
+  card.appendChild(hoursCard);
+  card.appendChild(locCard);
   contentDiv.appendChild(card);
+
+  sumCard.classList.add("sum");
+  hoursCard.classList.add("hours");
+  locCard.classList.add("location");
   card.classList.add("menu");
   title.classList.add("title");
 
+  sumCard.textContent =
+    "I love coming here withmy family I love Dib so much she makes the best stink pot pie! -Ethan";
+  hoursCard.textContent =
+    "Sunday: 8am - 8pm Monday: 6am - 6pm Tuesday: 6am - 6pm Wednesday: 6am - 6pm Thursday: 6am - 10pm Friday: 6am - 10pm Saturday: 8am - 10pm";
+  locCard.textContent = "10101992 peepee poopee drive hollywood CA";
   title.textContent = "Dib's";
   button1.textContent = "HOME";
   button2.textContent = "MENU";
