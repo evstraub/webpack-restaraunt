9fb01356302efc90bb3a9d51fa3d99b06c8601f1
Ethan Straub <evstraub@gmail.com>
2022-12-12 17:57:06 -0500

    added pages and changed formats

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import * as module from "./script.js";
+import { initialPageLoad } from "./initial-page-load.js";
+import { menu } from "./menu.js";
+import { contact } from "./contact.js";
+import "./style.css";
 
-console.log("hello webpack, youre very complicated!");
-module.initialPageLoad();
+initialPageLoad();
+
+// Tab switching module
+let tabSwitchingModule = (function () {
+  const homeTab = document.querySelector(".home");
+  homeTab.addEventListener("click", initialPageLoad);
+
+  const menuTab = document.querySelector(".menu");
+  menuTab.addEventListener("click", menu);
+
+  const contactTab = document.querySelector(".contact");
+  contactTab.addEventListener("click", contact);
+})();
